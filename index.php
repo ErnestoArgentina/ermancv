24aa162c8edfd344058ff831528689d853dda608
ermanac <ernestoargentina@gmail.com>
2019-03-11 13:54:47 +0200

    clash free table

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Ernesto Argentina Cumbe</title>
+    <meta name="theme-color" content="#002255">
+    <meta name="msapplication-navbutton-color" content="#002255">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#002255">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+    <link rel="stylesheet" href="css/lightbox.min.css" type="text/css">
+    <link rel="stylesheet" href="css/fontawesome.min.css" type="text/css">
+    <script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
+</head>
+<body> 
+   <!--------------------------------------------------> 
+    <main>
+        <div class="menu">
+            menu
+            <table> 
+                <tr><td><a href="#trabalho">TRABALHO</a></td><td><img src="images/icons/work.png"></td></tr>
+                <tr><td><a href="#educacao">EDUCACAO</a></td><td><img src="images/icons/timeline.png"></td></tr>
+                <tr><td><a href="#experiencia">EXPERIENCIA</a></td><td><img src="images/icons/work.png"></td></tr>
+                <tr><td><a href="#linguas">LINGUAS</a></td><td><img src="images/icons/linguas.png"></td></tr>
+            </table>
+        </div>
+        <header>
+            <div class="imagem">
+                <img src="images/img.jpg">
+            </div>
+            <div class="info-group">
+                <div class="info">
+                    <div class="nome col-50">
+                        <h1>ERNESTO ARGENTINA</h1>
+                        <h3>PROGRAMADOR</h3>
+                    </div>
+                    <div class="tabela-info col-50">
+                        <table>
+                            <tr><th></th><th></th><th></th></tr>
+                            <tr>
+                                <td><p>ernestoargentina@gmail.com</p></td><td><img src="images/icons/mail.png"></td>
+                            </tr>
+                            <tr>
+                                <td><p>843944019</p></td><td><img src="images/icons/tell.png"></td>
+                            </tr>
+                            <tr>
+                                <td><p>Ernesto Argentina Cumbe</p></td><td><img src="images/icons/face.png"></td>
+                            </tr>
+                            <tr>
+                                <td><p>erman_argentina</p></td><td><img src="images/icons/insta.png"></td>
+                            </tr>
+                            <tr>
+                                <td><p>er.quinate.com</p></td><td></td>
+                            </tr>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </header>   
+        <section>
+            <!-- esquerda -->
+            <div class="esquerda">
+                <div class="divisao" id="experiencia">
+                    <h3><b>EXPERIENCIA DE TRABALHO</b> <div class="icon"><img src="images/icons/work.png"></div><div class="sep"></div></h3>
+                    <table>
+                        <tr><th>Empresa</th><th>Profissao</th><th>Ano</th></tr>
+                        <tr>
+                         <td>Cilo criative</td><td>Edicao de video</td><td>2016</td>
+                        </tr>
+                        <tr>
+                         <td>Eric Photograph</td><td>Edicao de video</td><td>2018</td>
+                        </tr>
+                        <tr>
+                         <td>HARDTRACK</td><td>Desktop app developer</td><td>2018</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="divisao" id="pessoal">
+                    <h3><b>HABILIDADES PESSOAIS</b> <div class="icon"><img src="images/icons/timeline.png"></div><div class="sep"></div></h3>
+                    <table>
+                        <tr>
+                         <td><h2>></h2></td><td>PONTUALIDADE</td>
+                        </tr>
+                        <tr>
+                         <td><h2>></h2></td><td>OPTIMISMO</td>
+                        </tr>
+                        <tr>
+                         <td><h2>></h2></td><td>CRIATIVIDADE</td>
+                        </tr>
+                        <tr>
+                         <td><h2>></h2></td><td>SOCIATIVISMO</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="divisao profissionais" id="trabalho">
+                    <h3><b>HABILIDADES PROFISSIONAIS</b> <div class="icon"><img src="images/icons/work.png"></div><div class="sep"></div></h3>
+                     <div class="linguas">
+                        <label>JAVA</label>
+                        <div class="barra-bg"><div class="barra-per"></div></div>
+                    </div>
+                     <div class="linguas">
+                        <label>JAVASCRIPT</label>
+                        <div class="barra-bg"><div class="barra-per"></div></div>
+                    </div>
+                     <div class="linguas">
+                        <label>PASCAL</label>
+                        <div class="barra-bg"><div class="barra-per"></div></div>
+                    </div>
+                     <div class="linguas">
+                        <label>PHP</label>
+                        <div class="barra-bg"><div class="barra-per"></div></div>
+                    </div>
+                     <div class="linguas">
+                        <label>HTML & CSS</label>
+                        <div class="barra-bg"><div class="barra-per"></div></div>
+                    </div>
+                     <div class="linguas">
+                        <label>PYTHON</label>
+                        <div class="barra-bg"><div class="barra-per"></div></div>
+                    </div>
+                     <div class="linguas">
+                        <label>ANDROID</label>
+                        <div class="barra-bg"><div class="barra-per"></div></div>
+                    </div>
+                       <!--  <label>JAVA</label>
+                        <div class="barra-bg"><div class="barra-per"></div></div>
+                    </div> -->
+
+                </div>
+            </div><!-- FIM ESQUERDA -->
+
+
+            <!-- INICIO direita -->
+            <div class="direita">
+                <div class="divisao" id="linguas">
+                    <h3><b>LINGUAS</b> <div class="icon"><img src="images/icons/linguas.png"></div><div class="sep"></div></h3>
+                    <label>PORTUGUE</label>
+                    <div class="linguas">
+                        <b></b><b></b><b></b><b></b><b></b><b></b><i></i><i></i>
+                    </div>
+                    <label>INGLES</label>
+                    <div class="linguas">
+                        <b></b><b></b><b></b><b></b><i></i><i></i><i></i><i></i>
+                    </div>
+                    <label>CHANGANA</label>
+                    <div class="linguas">
+                        <b></b><b></b><b></b><b></b><b></b><b></b><b></b><i></i>
+                    </div>
+                    
+                </div>
+                <div class="divisao" id="educacao">
+                    <h3><b>EDUCACAO</b> <div class="icon"><img src="images/icons/timeline.png"></div><div class="sep"></div></h3>
+                     <p class="titulo-linha simples"><i class="dot"></i> <b>Primerio grau:</b> Escola primaria de Matchik-tchik </p>
+                    <p class="conteudo-linha "> Entre os anos 2003 e 2007</p>
+                        <p class="titulo-linha "><i class="dot"></i> <b>Segundo grau:</b> Escola primaria Polana Canico "B"</p>
+                        <p class="conteudo-linha "> Entre os anos 2008 e 2009 tendo terminado setima classe com media 13</p>
+                        <p class="titulo-linha "><i class="dot"></i> <b>Primerio ciclo:</b> Escola Secundaria Do Noroeste 1 </p>
+                                    <p class="conteudo-linha "> Entre os anos 2010 e 2013</p>
+                        <p class="titulo-linha "><i class="dot"></i> <b>Segundo ciclo:</b> Escola Secundaria Da Polana </p>
+                        <p class="conteudo-linha "> Entre os anos 2014 e 2015</p>
+                        <p class="titulo-linha "><i class="dot"></i> <b>Licenciatura em Informatica:</b> Universidade Eduardo Mondlane </p>
+                        <p class="conteudo-linha "> actualmente desde 2017</p>
+                </div>
+            </div> <!-- FIM DIREITA -->
+        </section> 
+    </main>    
+</body>
+</html>
